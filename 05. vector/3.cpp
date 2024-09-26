d3ef73f9fbf6b9cdf9aee01a3444e850bff7bfad
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-09-26 12:53:37 +0530

    Create 3.cpp

---
+++ 05. vector/3.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std ;
+
+
+int main ()
+{
+    vector <int> obj {2, 4,12, 10, 5, 7, 6, 15, 20,18, 3, 9} ; //main input
+    for (auto x:obj)
+        cout<<x<<" " ;
+    cout<<endl ;
+
+    vector<vector<int>> v ;
+    int i,j ;
+
+    for (i=0,j=0 ; i<obj.size() ; ) 
+    {
+        v.emplace_back() ;
+        v[j].emplace_back(obj[i++]) ; // i incremented by 1
+        while (i < obj.size() && obj[i-1]<obj[i])
+        {
+            v[j].emplace_back(obj[i]) ;
+            i++ ;
+        }
+        j++ ;
+    }
+
+    for(int m=0 ; m<v.size() ; m++)
+    {
+        for (auto n:v[m])
+            cout<<n<<" " ;
+        cout<<endl ;
+    }
+    
+}

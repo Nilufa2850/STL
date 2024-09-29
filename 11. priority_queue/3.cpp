93d3dafa01e9723aaea916be6c789ea1fba6bbb0
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-09-29 15:49:35 +0530

    Create 3.cpp

---
+++ 11. priority_queue/3.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std ;
+
+class Batsman
+{
+    private :
+        string name ;
+        int run,hundreds,fifties ;
+    public :
+        Batsman(string n, int r, int h, int f)
+        {
+            name = n ;
+            run = r ;
+            hundreds = h ;
+            fifties = f ;
+        }
+        
+        string getName (){return name;}
+        int getRun (){return run ;}
+        int get100 (){ return hundreds ;}
+        int get50 (){return fifties ;}
+
+        void display ()
+        {
+            cout<<endl<<name<<" : Run="<<run<<" 100="<<hundreds<<" 50="<<fifties ;
+        }
+} ;
+
+struct comByRun
+{
+    bool operator()(Batsman &b , Batsman &c )
+    {
+        return (b.getRun() < c.getRun()) ;
+        //max heap
+    }
+};
+
+int main ()
+{
+    priority_queue <Batsman,vector<Batsman> , comByRun> pq ;
+
+    pq.emplace("Asif",150,2,6) ;
+    pq.emplace("Saha",28,2,6) ;
+    pq.emplace("nil",291,2,6) ;
+    pq.emplace("vai",47,2,6) ;
+    pq.emplace("abbu",87,2,6) ;
+
+    while (!pq.empty())
+    {
+        Batsman temp = pq.top() ;
+        pq.pop() ;
+        temp.display() ;
+    }
+
+
+}
